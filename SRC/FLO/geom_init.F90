ea93961b4bcad763d2c02b5eb39055b751f787c9
Mohammad Mehrabadi <mehr@iastate.edu>
2015-05-19 14:53:08 -0500

    geom_init

--- SRC/FLO/geom_init.F90
+++ SRC/FLO/geom_init.F90
@@ -28,45 +28,42 @@ CONTAINS
 !!$c     ********************************************************************
 		nbnd=0
 		nrpr=0
-		theta=0.
-		phi=0.
+		theta = zero
+		phi = zero
 		nbnd=0
 
 		!********************************************************************
 		! generate flow reversal pairs
 		!     goto 100
 
-		ri=r-dr
-		ro=r+dr
-		ro2 = r+two*dr
-
-		theta=0.
-		phi=0.
-		imax=NINT(twopi*r/four*f2)
-
+		!ri=r-dr
+		!ro=r+dr
+		!ro2 = r+two*dr
+		!theta = zero
+		!phi = zero
+		!imax=NINT(twopi*r/four*f2)
 		!imax = 30
-		dtheta=twopi/imax/four
-		nbndloc = 0
-		nrprloc = 0
-
-		DO i=1,imax+1
-			jmax=NINT(twopi*r*SIN(theta)/4*f2)
-			!jmax = 30
-			dphi=twopi/jmax/4
-			!Print*,'dphi  = ', dphi, r*SIN(theta)*SIN(dphi)
-			phi=0.
-			DO j=1,jmax+1
-				rado = xo2**2. + yo2**2. + zo2**2.
-				r2=x*x+y*y+z*z
-
-				nbndloc=nbndloc+1
-				phi=phi+dphi
-				nrprloc=nrprloc+1
-			ENDDO
-			!read(*,*)
-			theta=theta+dtheta
-		ENDDO
-		!PRINT*,'nbndloc 1= ', nbndloc
+		!dtheta=twopi/imax/four
+		!nbndloc = 0
+		!nrprloc = 0
+		!DO i=1,imax+1
+		!	jmax=NINT(twopi*r*SIN(theta)/4*f2)
+		!	!jmax = 30
+		!	dphi=twopi/jmax/4
+		!	!Print*,'dphi  = ', dphi, r*SIN(theta)*SIN(dphi)
+		!	phi = zero
+		!	DO j=1,jmax+1
+		!		rado = xo2**2. + yo2**2. + zo2**2.
+		!		r2=x*x+y*y+z*z
+		!
+		!		nbndloc=nbndloc+1
+		!		phi=phi+dphi
+		!		nrprloc=nrprloc+1
+		!	ENDDO
+		!	!read(*,*)
+		!	theta=theta+dtheta
+		!ENDDO
+		!!PRINT*,'nbndloc 1= ', nbndloc
 		ALLOCATE(nxb(nbndloc), nyb(nbndloc), nzb(nbndloc))
 		!ALLOCATE(nxi(nbndloc), nyi(nbndloc), nzi(nbndloc))
 		!ALLOCATE(nxo(nbndloc), nyo(nbndloc), nzo(nbndloc))
@@ -76,81 +73,82 @@ CONTAINS
 		ro=r+dr
 		ro2 = r+two*dr
     
-		theta=0.
-		phi=0.
 		imax=NINT(twopi*r/four*f2)
+		dtheta=twopi/imax/four
 
+		theta = zero
+		phi   = zsero
 		nbndloc = 0
 		nrprloc = 0
-
-		!Print*,'dtheta  = ', dtheta,imax
 		DO i=1,imax+1
 			jmax=NINT(twopi*r*SIN(theta)/4*f2)
 			!jmax = 30
 			dphi=twopi/jmax/4
 			!Print*,'dphi  = ', dphi, r*SIN(theta)*SIN(dphi)
 
-			phi=0.
+			phi = zero
 			DO j=1,jmax+1
-				!^^^^^^^^^^^ Mohammad 08-18-2010 ^^^^^^^^^^^^^^^^^^^^^^^^^		
-				! changing the spherical coordinates direction to have the 
-				! axis of the sphere aligned with the flow direction
 				z=r*COS(theta)/r
-				IF(ABS(z).LT.1.0d-5) z=0.
+				IF(ABS(z) < small_number) z = zero
 				y=r*SIN(theta)*COS(phi)/r
-				IF(ABS(y).LT.1.0d-5) y=0.
+				IF(ABS(y) < small_number) y = zero
 				x=r*SIN(theta)*SIN(phi)/r
-				IF(ABS(x).LT.1.0d-5) x=0.
+				IF(ABS(x) < small_number) x = zero
 				r2=x*x+y*y+z*z
 
 				zi=ri*COS(theta)
-				IF(ABS(zi).LT.1.0d-5) zi=0.
+				IF(ABS(zi) < small_number) zi = zero
 				yi=ri*SIN(theta)*COS(phi)
-				IF(ABS(yi).LT.1.0d-5) yi=0.
+				IF(ABS(yi) < small_number) yi = zero
 				xi=ri*SIN(theta)*SIN(phi)        
-				IF(ABS(xi).LT.1.0d-5) xi=0.
+				IF(ABS(xi) < small_number) xi = zero
+
 				zo=ro*COS(theta)
-				IF(ABS(zo).LT.1.0d-5) zo=0.
+				IF(ABS(zo) < small_number) zo = zero
 				yo=ro*SIN(theta)*COS(phi)
-				IF(ABS(yo).LT.1.0d-5) yo=0.
+				IF(ABS(yo) < small_number) yo = zero
 				xo=ro*SIN(theta)*SIN(phi)        
-				IF(ABS(xo).LT.1.0d-5) xo=0.
+				IF(ABS(xo) < small_number) xo = zero
 
 				zo2=ro2*COS(theta)
-				IF(ABS(zo2).LT.1.0d-5) zo2=0.
+				IF(ABS(zo2) < small_number) zo2 = zero
 				yo2=ro2*SIN(theta)*COS(phi)
-				IF(ABS(yo2).LT.1.0d-5) yo2=0.
+				IF(ABS(yo2) < small_number) yo2 = zero
 				xo2=ro2*SIN(theta)*SIN(phi)        
-				IF(ABS(xo2).LT.1.0d-5) xo2=0.
+				IF(ABS(xo2) < small_number) xo2 = zero
 
-!		x=r*COS(theta)/r
-!		IF(ABS(x).LT.1.0d-5) x=0.
-!		y=r*SIN(theta)*COS(phi)/r
-!		IF(ABS(y).LT.1.0d-5) y=0.
-!		z=r*SIN(theta)*SIN(phi)/r
-!		IF(ABS(z).LT.1.0d-5) z=0.
-!		r2=x*x+y*y+z*z
-!
-!		xi=ri*COS(theta)
-!		IF(ABS(xi).LT.1.0d-5) xi=0.
-!		yi=ri*SIN(theta)*COS(phi)
-!		IF(ABS(yi).LT.1.0d-5) yi=0.
-!		zi=ri*SIN(theta)*SIN(phi)        
-!		IF(ABS(zi).LT.1.0d-5) zi=0.
-!
-!		xo=ro*COS(theta)
-!		IF(ABS(xo).LT.1.0d-5) xo=0.
-!		yo=ro*SIN(theta)*COS(phi)
-!		IF(ABS(yo).LT.1.0d-5) yo=0.
-!		zo=ro*SIN(theta)*SIN(phi)        
-!		IF(ABS(zo).LT.1.0d-5) zo=0.
-!
-!		xo2=ro2*COS(theta)
-!		IF(ABS(xo2).LT.1.0d-5) xo2=0.
-!		yo2=ro2*SIN(theta)*COS(phi)
-!		IF(ABS(yo2).LT.1.0d-5) yo2=0.
-!		zo2=ro2*SIN(theta)*SIN(phi)        
-!		IF(ABS(zo2).LT.1.0d-5) zo2=0.
+				!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^		
+				! changing the spherical coordinates direction to have the 
+				! axis of the sphere aligned with the flow direction
+				!x=r*COS(theta)/r
+				!IF(ABS(x) < small_number) x = zero
+				!y=r*SIN(theta)*COS(phi)/r
+				!IF(ABS(y) < small_number) y = zero
+				!z=r*SIN(theta)*SIN(phi)/r
+				!IF(ABS(z) < small_number) z = zero
+				!r2=x*x+y*y+z*z
+				!
+				!xi=ri*COS(theta)
+				!IF(ABS(xi) < small_number) xi = zero
+				!yi=ri*SIN(theta)*COS(phi)
+				!IF(ABS(yi) < small_number) yi = zero
+				!zi=ri*SIN(theta)*SIN(phi)        
+				!IF(ABS(zi) < small_number) zi = zero
+				!
+				!xo=ro*COS(theta)
+				!IF(ABS(xo) < small_number) xo = zero
+				!yo=ro*SIN(theta)*COS(phi)
+				!IF(ABS(yo) < small_number) yo = zero
+				!zo=ro*SIN(theta)*SIN(phi)        
+				!IF(ABS(zo) < small_number) zo = zero
+				!
+				!xo2=ro2*COS(theta)
+				!IF(ABS(xo2) < small_number) xo2 = zero
+				!yo2=ro2*SIN(theta)*COS(phi)
+				!IF(ABS(yo2) < small_number) yo2 = zero
+				!zo2=ro2*SIN(theta)*SIN(phi)        
+				!IF(ABS(zo2) < small_number) zo2 = zero
+				!------------------------------------------------------------
 
 				rado = xo2**2. + yo2**2. + zo2**2.
 				r2=x*x+y*y+z*z
@@ -161,17 +159,15 @@ CONTAINS
 				nzb(nbndloc) = z 
 				nyb(nbndloc) = y 
 				nxb(nbndloc) = x 
-!!$          nzi(nbndloc) = zi 
-!!$          nyi(nbndloc) = yi 
-!!$          nxi(nbndloc) = xi 
-!!$          nzo(nbndloc) = zo 
-!!$          nyo(nbndloc) = yo 
-!!$          nxo(nbndloc) = xo 
-!!$
-!!$          nzo2(nbndloc) = zo2 
-!!$          nyo2(nbndloc) = yo2 
-!!$          nxo2(nbndloc) = xo2
-
+				!!$nzi(nbndloc) = zi 
+				!!$nyi(nbndloc) = yi 
+				!!$nxi(nbndloc) = xi 
+				!!$nzo(nbndloc) = zo 
+				!!$nyo(nbndloc) = yo 
+				!!$nxo(nbndloc) = xo 
+				!!$nzo2(nbndloc) = zo2 
+				!!$nyo2(nbndloc) = yo2 
+				!!$nxo2(nbndloc) = xo2
 			ENDDO
 			!read(*,*)
 			theta=theta+dtheta
